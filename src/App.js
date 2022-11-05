4fc0ce85b54a5620b0d29902be89675361a7b744
victor <82973437+chipingo42@users.noreply.github.com>
2022-11-05 14:39:37 +0100

    feature_navbar and darkmode

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './Components/Navbar'
+
+
+function App() {
+  return (
+    <div className="w-full h-[100vh] dark:bg-[#151D3B] transition duration-500">
+     <Navbar />
+    </div>
+  );
+}
+
+export default App;
