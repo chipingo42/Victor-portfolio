c456efa318ce73763485203a0823af52756f6843
victor <82973437+chipingo42@users.noreply.github.com>
2022-11-09 01:42:13 +0100

    rebasing my navbar code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero';
+import { Route, Routes } from 'react-router-dom';
 
 
 
 function App() {
   return (
     <div className="w-full h-[100vh] dark:bg-[#151D3B] transition duration-500">
-     <Navbar />
-     <Hero />
+      <Routes>
+        <Route path='/' element={<Navbar />}/>
+        <Route path='/' element={<Hero />}/>
+      </Routes>
+     {/* <Navbar />
+     <Hero /> */}
     </div>
     
   );
